6283f753f102f823d34838d556d0dcb325cd80a2
bolavefasfas <82230241+bolavefasfas@users.noreply.github.com>
2021-04-09 22:26:35 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -98,7 +98,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded using 𝗔𝗭𝗠𝗶𝗿𝗿𝗼𝗿',
             'mimeType': mime_type,
         }
         if parent_id is not None:
